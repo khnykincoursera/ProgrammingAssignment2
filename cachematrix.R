4d56c7104e912fdabbb3db7ad2099c0e9d48e372
khnykincoursera <khnykin@gmail.com>
2015-01-25 16:39:45 +0300

    Task submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## The function accepts a matrix and initializes internal variables
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversion <- NULL
+        set <- function(y) {
+                x <<- y
+                inversion <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) inversion <<- solve
+        getsolve <- function() inversion
+        list(set = set,
+             get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## The function returns inverse of a matrix
+## Any other parameters to solve function  except for the matrix itself are ignored
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        ## do we have a result already?
+        inversion <- x$getsolve()
+        if(!is.null(inversion)) {
+		## cache hit, then take cached result and return it
+                return(inversion)
+        }
+
+        ## cache miss, then retrieve data and cache it
+        data <- x$get()
+        ## to find inverse of a matrix we do not need other parameters (and we did not remember them)
+        inversion <- solve(data)
+        x$setsolve(inversion)
+        inversion
 }
